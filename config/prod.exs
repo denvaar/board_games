75b4adb562195f5b08078440f0c86239ec5bb690
Denver Smith <denverpsmith@gmail.com>
2021-01-16 23:58:37 -0700

    update prod host

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :board_games, BoardGamesWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: System.get_env("HOSTNAME"), port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule BoardGames.MixProject do
     [
       app: :board_games,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.11.3",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
